9d0d7fa6274f6156eb1281086482315b65f8e6fd
joshyeom <jhyeom95@daum.net>
2022-12-08 11:01:04 +0900

    refresh

---
+++ Server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const todoRouter = require('./routes/todo');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', todoRouter); // 기본주소: localhost:PORT/api
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
