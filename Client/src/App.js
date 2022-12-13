d1226345639f71b15d7debd26a54668a6b54f845
joshyeom <jhyeom95@daum.net>
2022-12-13 12:02:05 +0900

    feat:Todo 삭제 기능 구현

--- Client/src/App.js
+++ Client/src/App.js
@@ -26,11 +26,18 @@ const App = () => {
   // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
   // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
   const addItem = (newItem) => {
-    // newItem - {id: ?, title: ?, done: false}
+    // newItem - {id: xx, title: xx, done: false}
     newItem.id = todoId.current++; // key를 위한 id 설정
     newItem.done = false; // done 초기화
     // 기존 todoItems를 유지하고, 새로운 newItem을 추가
     setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+  };
+
+  // 전체 Todo 리스트(todoItems)는 App 컴포넌트에서 관리하고 있으므로
+  // deleteItem() 함수는 App 컴포넌트에 작성해야 함
+  const deleteItem = (targetItem) => {
+    let newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
+    setTodoItems(newTodoItems);
   };
 
   return (
@@ -38,7 +45,7 @@ const App = () => {
       <AddTodo addItem={addItem} />
       {todoItems.map((item) => {
         // console.log(item); // {id: 1, title: 'My Todo1', done: false}
-        return <Todo key={item.id} item={item} />;
+        return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
       })}
     </div>
   );
--- Client/src/components/Todo.js
+++ Client/src/components/Todo.js
@@ -1,14 +1,25 @@
-const Todo = ({ item }) => {
+import { useState } from "react";
+
+const Todo = ({ item, deleteItem }) => {
+  // console.log(item); // { id: 1, title: 'todo1', done: false, }
+  const { id, title, done } = item;
+  const [todoItem, setTodoItem] = useState(item);
+
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+  };
+
   return (
     <div className="Todo">
       <input
         type="checkbox"
-        id={`todo${item.id}`}
-        name={`todo${item.id}`}
-        value={`todo${item.id}`}
-        defaultChecked={item.done}
+        id={`todo${id}`}
+        name={`todo${id}`}
+        value={`todo${id}`}
+        defaultChecked={done}
       />
-      <label htmlFor={`todo${item.id}`}>{item.title}</label>
+      <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteBtnClick}>DELETE</button>
     </div>
   );
 };
